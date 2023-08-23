d7447d66797a79600981a05a637460f32d34bbcf
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-23 13:16:19 -0400

    adds view for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   include RestrictAccess
-  def show; end
+  def show
+    @products = Product.all
+    @categories = Category.all
+  end
 end
