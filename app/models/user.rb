a4be779245534e3efaee4d54e9adf14b1153ee01
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-28 17:24:06 -0400

    adds class method authenticate_with_credentials on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 8 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    return unless user&.authenticate(password)
+
+    user
+  end
 end
