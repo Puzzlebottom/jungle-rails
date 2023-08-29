e53098f95d2e8fc0c88e8045b998f900134e3d6f
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-08-29 16:28:00 -0400

    adds cypress tests for the home route

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,17 @@
+describe('homepage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('should load the homepage', () => {
+    cy.get('.title').should('exist');
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products > article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+});
+
